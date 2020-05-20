235654290a60e603b75019c0d5ae0efdedf430d7
CHAKRAVARTHI MONARCH <chakravarthimonarch278@gmail.com>
2020-05-20 15:41:46 +0530

    First commit

---
+++ class1/1-class.py
@@ -0,0 +1,8 @@
+money = 66
+item = 60
+choclate = 20
+if(money-item > 0):
+    if(money-item > choclate):
+        print("Yes we can buy choclate")
+    else:
+        print("No you can't buy choclate")
---
+++ class2/2-class.py
@@ -0,0 +1,43 @@
+# for x in "chakravarthi",end="":
+#     print(x)
+# n=10
+# i=0
+# while(i<n):
+#     print(i)
+#     i+=1
+# for i in range(1,10):
+#     if i==4:
+#         continue
+#     print(i)
+# print("Terminated the if loop")    
+# for i in range(5,6):
+#     for j in range(1,5):
+#         print(i*j,end="\n")
+# print("Enter the number :")
+# n=int(input())
+# sum=0
+# for i in range(n+1) :
+#  sum+=i
+# print("sum is :",end="")  
+# print(sum)         
+# i = 2
+# while True:
+#     if i%3 == 0:
+#         break
+#     print(i)
+#     i += 2
+# True = False
+# while True:
+#     print(True)
+#     break
+# i = 1
+# while True:
+#     if i%0O7 == 0:
+#         break
+#     print(i)
+#     i += 1
+sum=0
+n=int(input())
+for i in range (1,n) :
+    sum+=i**2
+print(sum)
